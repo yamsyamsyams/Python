77e46280d0360f063e83946a98658f71a494696e
James Hoang <j.hoang2080@gmail.com>
2020-08-24 21:40:33 -0400

    python practice from automate the boring stuff

---
+++ Practice/rpsGame.py
@@ -0,0 +1,13 @@
+import random, sys
+
+print('RORCK, PAPER, SCISSORS')
+
+# These variables keep track of the number of wins, losses, and ties.
+wins = 0
+losses = 0
+ties = 0
+
+while True: #The main game loop.
+    print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
+    while True: # The player input loop.
+        
